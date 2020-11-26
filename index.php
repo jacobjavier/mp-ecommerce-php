e26e87912194ec8cf794dde081f4ac02cea169f6
jacobjavier <jacobjavier95@gmail.com>
2020-11-26 00:08:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,36 @@
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     
     <meta name="viewport" content="width=1024">
-    <title>Tienda e-commerce</title>
+    <title>Tienda e-commerce Jacob</title>
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="format-detection" content="telephone=no">
 
+    
     <script
     src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
     crossorigin="anonymous"></script>
+    
+    <?php
+// SDK de Mercado Pago
+require __DIR__ .  '/mercadosdk/vendor/autoload.php';
+
+// Agrega credenciales
+MercadoPago\SDK::setAccessToken('APP_USR-2380553435167399-112600-354775258b16a065a835390cddd5376a-669067675');
+
+// Crea un objeto de preferencia
+$preference = new MercadoPago\Preference();
+
+// Crea un ítem en la preferencia
+$item = new MercadoPago\Item();
+$item->title = 'Producto de jacob';
+$item->quantity = 1;
+$item->unit_price = 795.56;
+$preference->items = array($item);
+$preference->save();
+?>
+
 
     <link rel="stylesheet" href="./assets/category-landing.css" media="screen, print">
 
